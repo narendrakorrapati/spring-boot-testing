914a66550a6a817e62dc1329a584cc52e6411303
narendrakorrapati <korrapatinarendra843@gmail.com>
2023-07-16 16:40:17 +0530

    base setup

---
+++ spring-boot-testing/src/test/java/com/mnt/springboot/SpringBootTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnt.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
